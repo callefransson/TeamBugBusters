d73bc3d3d965b4aefdcbd8e436f238455abca853
calle fransson <calle-fransson@hotmail.com>
2024-05-07 13:35:09 +0200

    Add project files.

---
+++ TeamBugBusters/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TeamBugBusters.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
