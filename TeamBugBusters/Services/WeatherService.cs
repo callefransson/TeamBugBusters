15aada54ff0b762837b3e45afe485e9c3a79c17b
calle fransson <calle-fransson@hotmail.com>
2024-05-28 22:47:56 +0200

    Removed home page

---
+++ TeamBugBusters/Services/WeatherService.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace TeamBugBusters.Services
+{
+    public class WeatherService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+
+        public WeatherService(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _apiKey = configuration["WeatherApiKey"];
+        }
+
+        public async Task<WeatherInfo> GetWeatherAsync(string city)
+        {
+            var response = await _httpClient.GetAsync($"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={_apiKey}&units=metric");
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            var weatherInfo = JsonConvert.DeserializeObject<WeatherInfo>(json);
+
+            return weatherInfo;
+        }
+    }
+    public class WeatherInfo
+    {
+        public Main Main { get; set; }
+        public List<Weather> Weather { get; set; }
+    }
+
+    public class Main
+    {
+        public double Temp { get; set; }
+    }
+
+    public class Weather
+    {
+        public string Description { get; set; }
+    }
+}
